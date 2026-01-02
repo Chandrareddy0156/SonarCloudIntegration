d05268d0e0ba41671fb7b57bd32f4d19d780c64f
Annem Chandra Shekhar <chandra.annem@zemosolabs.com>
2026-01-02 07:07:08 +0530

    changed file

--- blinkist/src/simple.tsx
+++ blinkist/src/simple.tsx
@@ -6,30 +6,20 @@ type Props = {
 
 const SonarTest: React.FC<Props> = ({ title }) => {
   const [count, setCount] = useState(0);
- 
 
-  
   const increment = () => {
-    if (count > 5) {
-      console.log("Count is greater than 5");
-    }
-
     setCount(count + 1);
   };
 
   useEffect(() => {
     console.log("Component mounted");
-  }); 
-
- 
-  
+  });
 
   return (
     <>
       <h2>{title}</h2>
 
       <p>Count: {count}</p>
-     
 
       <button onClick={increment}>Increment</button>
     </>
